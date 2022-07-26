6772cffc32db1f720f95e69c1227a81675bc5182
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-26 18:51:08 +0200

    feat: add new book to DOM

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
+const cards = document.querySelector(".cards");
+
+let myLibrary = [];
+
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.info = function () {
-    let readInfo = this.isRead ? "read" : "not read yet";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readInfo}.`;
-  };
+}
+
+Book.prototype.info = function () {
+  let readInfo = this.isRead ? "read" : "not read yet";
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readInfo}.`;
+};
+
+function addBookToLibrary(book) {
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("card");
+
+  let bookTitle = document.createElement("h2");
+  bookTitle.classList.add("card__title");
+  bookTitle.textContent = book.title;
+
+  let bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("card__author");
+  bookAuthor.textContent = book.author;
+
+  let bookPages = document.createElement("p");
+  bookPages.classList.add("card__pages");
+  bookPages.textContent = book.pages;
+
+  let bookStatus = document.createElement("div");
+  bookStatus.classList.add("card__status");
+  bookStatus.textContent = book.isRead ? "x" : "";
+
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookStatus);
+
+  cards.appendChild(bookCard);
+
+  myLibrary.push(book);
 }
