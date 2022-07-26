96fd33d0b257f07c4d01b50fd2b979fe82b07dbc
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-26 16:59:27 +0200

    feat: add book constructor

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = function () {
+    let readInfo = this.isRead ? "read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readInfo}.`;
+  };
+}
